c42c4e5fb4090d7834cca67a6b614a4cc8e61f98
GaniraDennis <ganiradennis99@gmail.com>
2023-04-18 10:30:01 +0300

    pull

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,26 +1,20 @@
+var picNum = 0;
+var count_slide = $('#carouselExampleFade .carousel-item').length - 1;
 
-
-document.body.style.backgroundImage = "url(./Assets/home background.jpg)";
-
-document.body.style.backgroundImage = "url(./Assets/home background.jpg)";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundPosition = "center";
-
-
-document.body.classList.add('bg-image');
-
-const navLinks = document.querySelectorAll('.nav-link');
-
-navLinks.forEach(link => {
-  link.addEventListener('click', function() {
-   
-    navLinks.forEach(link => {
-      link.classList.remove('active');
-    });
-    
-  
-    this.classList.add('active');
-  });
+$(".carousel-control-next").click(function() {
+  $("#ed-body").removeClass("bg-img" + picNum);
+    picNum++;
+    if (picNum > count_slide) {
+      picNum = 0;
+    }
+  $("#ed-body").addClass("bg-img" + picNum);
 });
 
-
+$(".carousel-control-prev").click(function() {
+  $("#ed-body").removeClass("bg-img" + picNum);
+    picNum--;
+    if (picNum < 0) {
+      picNum = count_slide;
+    }
+  $("#ed-body").addClass("bg-img" + picNum);
+});
